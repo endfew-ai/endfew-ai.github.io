246cc4d8b5199d0db5908eec1cac43673f8274a9
endfew <endfew@gmail.com>
2026-07-19 02:29:15 +0800

    feat: add academy information panel modules

---
+++ assets/ReactionResultSurface-CTwYPuTO.js
@@ -0,0 +1,1 @@
+import{t as e}from"./vendor-react-Dh4-mYnC.js";import{qo as t}from"./game-data-D08Ik-nX.js";var n=e();function r({tone:e}){return(0,n.jsx)(`img`,{alt:``,"aria-hidden":`true`,className:`reaction-result-surface`,decoding:`async`,draggable:`false`,fetchPriority:`low`,onError:e=>{e.currentTarget.hidden=!0},src:t.reactionResultSurfaces[e]})}export{r as t};
