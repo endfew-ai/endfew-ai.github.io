943062f0133170041edf71d532b823c7d4f540b1
endfew <endfew@gmail.com>
2026-07-18 23:22:27 +0800

    feat: add gold ink brush title modules

--- assets/ReactionResultSurface--Bv13N6K.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./vendor-react-CU2IpwZ4.js";import{qo as t}from"./game-data-CVJCRiB0.js";var n=e();function r({tone:e}){return(0,n.jsx)(`img`,{alt:``,"aria-hidden":`true`,className:`reaction-result-surface`,decoding:`async`,draggable:`false`,fetchPriority:`low`,onError:e=>{e.currentTarget.hidden=!0},src:t.reactionResultSurfaces[e]})}export{r as t};
