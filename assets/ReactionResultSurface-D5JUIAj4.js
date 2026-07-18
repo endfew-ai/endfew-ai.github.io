640a5f1d2bf2e93c68a58d9e0537707757ad21e7
endfew <endfew@gmail.com>
2026-07-18 15:12:21 +0800

    新增指尖定位答案判定特效

--- assets/ReactionResultSurface-D5JUIAj4.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./vendor-react-CrUN5DsL.js";import{Ko as t}from"./game-data-UQ9gwT-m.js";var n=e();function r({tone:e}){return(0,n.jsx)(`img`,{alt:``,"aria-hidden":`true`,className:`reaction-result-surface`,decoding:`async`,draggable:`false`,fetchPriority:`low`,onError:e=>{e.currentTarget.hidden=!0},src:t.reactionResultSurfaces[e]})}export{r as t};
